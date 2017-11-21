d08e5d459db1a283cddd512fef7d7122ca206618
Unknown <Sean.Purvis@usm.edu>
2017-11-21 17:10:00 -0600

    Change User Schema

--- server/models/user.js
+++ server/models/user.js
@@ -8,6 +8,9 @@ a timestamped database migration file.
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
     username: { type: DataTypes.STRING },
+    email: { type: DataTypes.STRING },
+    firstName: { type: DataTypes.STRING },
+    lastName: { type: DataTypes.STRING },
     password: { type: DataTypes.STRING },
     admin: { type: DataTypes.BOOLEAN },
   });
