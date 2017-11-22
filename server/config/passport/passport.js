9287fbdd30f27e215ca8ec2c435f60921b7832f7
Will Jackson <wrejcodes@gmail.com>
2017-11-22 10:00:37 -0600

    Added local strategy for login routes yeahhhh

--- server/config/passport/passport.js
+++ server/config/passport/passport.js
@@ -1,8 +1,11 @@
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
+const LocalStrategy = require('passport-local').LocalStrategy;
 
 const User = require('../../models').User;
 const config = require('../config.js');
+
+const bcrypt = require('bcrypt');
 
 module.exports = function(passport) {
   const opts = {
@@ -27,4 +30,16 @@ module.exports = function(passport) {
         return done(err);
       });
   }));
+
+  passport.use(new LocalStrategy((username, password, done) =>{
+    User.findOne({username: username}, (err, user)=> {
+      if err return done(err);
+      if !user return done(null, false);
+      bcrypt.compare(password, user.password).then((res,err) => {
+        if !res return done(null, false);
+        if res return done(null, user);
+        if err return done(err);
+      })
+    })
+  })
 };
