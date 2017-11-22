875c59a79d412949851b6ee932e31f2741f90422
Will Jackson <wrejcodes@gmail.com>
2017-11-22 10:39:22 -0600

    unrubyized my if statements on passport.js file

--- server/config/passport/passport.js
+++ server/config/passport/passport.js
@@ -33,12 +33,12 @@ module.exports = function(passport) {
 
   passport.use(new LocalStrategy((username, password, done) =>{
     User.findOne({username: username}, (err, user)=> {
-      if err return done(err);
-      if !user return done(null, false);
+      if (err) return done(err);
+      if (!user) return done(null, false);
       bcrypt.compare(password, user.password).then((res,err) => {
-        if !res return done(null, false);
-        if res return done(null, user);
-        if err return done(err);
+        if (!res) return done(null, false);
+        if (res) return done(null, user);
+        if (err) return done(err);
       })
     })
   })
