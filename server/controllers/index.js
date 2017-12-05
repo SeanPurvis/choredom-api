f6fb2f2e4720080386cc4da966f7afa9f010ce57
Unknown <Sean.Purvis@usm.edu>
2017-12-05 14:43:20 -0600

    Clean code

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,13 +1,9 @@
-const todos = require('./todos');
-const todoItems = require('./todoitems')
 const users = require('./users');
 const chores = require('./chores');
 const groups = require('./groups');
 const groupsusers = require('./groupsusers');
 const groupsettings = require('./groupsettings');
 module.exports = {
-  todos,
-  todoItems,
   users,
   chores,
   groups,
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,3 @@
-const todosController = require('../controllers').todos;
-const todoItemsController = require('../controllers').todoItems;
 const usersController = require('../controllers').users;
 const choresController = require('../controllers').chores;
 const groupsController = require('../controllers').groups;
@@ -10,22 +8,8 @@ const groupsettingsController = require('../controllers').groupsettings;
 // This file exists to map controllers to API endpoints
 module.exports = (app, passport, jwt) => {
   app.get('/api/v1/', (req, res) => res.status(200).send({
-    message: 'Welcome to the API',
+    message: 'Welcome to the API, you have succesfully logged in.',
   }));
-
-// Todo routes
-  app.post('/api/v1/todos', todosController.create);
-  app.get('/api/v1/todos', todosController.list);
-  app.get('/api/v1/todos/:todoId', todosController.retrieve);
-  app.put('/api/v1/todos/:todoId', todosController.update);
-  app.delete('/api/v1/todos/:todoId', todosController.destroy);
-
-// TodoItem routes
-  app.post('/api/v1/todos/:todoId/items', todoItemsController.create);
-  app.put('/api/v1/todos/:todoId/items/:todoItemId', todoItemsController.update);
-  app.delete(
-    '/api/v1/todos/:todoId/items/:todoItemId', todoItemsController.destroy
-  );
 
 // User routes
 app.post('/api/v1/users', usersController.create);
@@ -34,36 +18,35 @@ app.get('/api/v1/users/:userId', usersController.retrieve);
 app.put('/api/v1/users/:userId', usersController.update);
 app.delete('/api/v1/users/:userId', usersController.destroy);
 
-// User routes
+// Chore routes
 app.post('/api/v1/chores', choresController.create);
 app.get('/api/v1/chores', choresController.list);
 app.get('/api/v1/chores/:choreId', choresController.retrieve);
 app.put('/api/v1/chores/:choreId', choresController.update);
 app.delete('/api/v1/chores/:choreId', choresController.destroy);
 
+// GroupSettings routes
 app.post('/api/v1/groupsettings', groupsettingsController.create);
 app.get('/api/v1/groupsettings', groupsettingsController.list);
 app.get('/api/v1/groupsettings/:groupsettingsId', groupsettingsController.retrieve);
 app.put('/api/v1/groupsettings/:groupsettingsId', groupsettingsController.update);
 app.delete('/api/v1/groupsettings/:groupsettingsId', groupsettingsController.destroy);
 
+// Group Routes
 app.post('/api/v1/groups', groupsController.create);
 app.get('/api/v1/groups', groupsController.list);
 app.get('/api/v1/groups/:groupsId', groupsController.retrieve);
 app.put('/api/v1/groups/:groupsId', groupsController.update);
 app.delete('/api/v1/groups/:groupsId', groupsController.destroy);
 
+// GroupsUsers routes
 app.post('/api/v1/groupsusers', groupsusersController.create);
 app.get('/api/v1/groupsusers', groupsusersController.list);
 app.get('/api/v1/groupsusers/:groupsusersId', groupsusersController.retrieve);
 app.put('/api/v1/groupsusers/:groupsusersId', groupsusersController.update);
 app.delete('/api/v1/groupsusers/:groupsusersId', groupsusersController.destroy);
 
-
-
 // Login route
-
-
 app.post('/api/v1/login', (req,res,next)=> {
   passport.authenticate('local', (err, user, info)=>{
     if(err){
@@ -94,10 +77,4 @@ app.post('/api/v1/login', (req,res,next)=> {
     })
   })(req,res,next)
 });
-
-// For any other request on TodoItems
-  app.all('/api/v1/todos/:todoId/items', (req, res) =>
-    res.status(405).send({
-      message: "Method Not Allowed",
-    }));
 };
